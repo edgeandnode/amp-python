21c796e1d6f83ebe30b156f6bf015535312e99ea
Vivian Peng <vpeng666@gmail.com>
2025-12-12 07:36:30 +0900

    Revised the commit

--- models/staging/stg_test.sql
+++
@@ -1,6 +0,0 @@
-{{ config(
-    dependencies={'eth': '_/eth_firehose@1.0.0'},
-    track_progress=True,
-    track_column='block_num'
-) }}
-SELECT block_num, tx_hash FROM {{ ref('eth') }}.logs LIMIT 10
